46d64a5966add169b4eb47ebdb0366e0e28c7e04
Edgar <edgarasige@gmail.com>
2024-04-06 07:02:11 +0300

    Completed assingments

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+        if (array[i] + array[j] === target) {
+            return true;
+        }
+    }
+}
+return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Time Complexity: O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+function hasTargetSum(arr, target):
+    Initialize an empty map
+    
+    for each number in arr:
+        Calculate complement = target - number
+        
+        If complement exists in the map:
+            Return true
+        
+        Add the current number to the map
+        
+    Return false
 */
 
 /*
-  Add written explanation of your solution here
+  function hasTargetSum takes an array of numbers and a target sum as input.
+  It iterates through the array and checks if there are two numbers that sum up to the target.
+  If such a pair is found, the function returns true; otherwise, it returns false.
+
+
 */
 
 // You can run `node index.js` to view these console logs
